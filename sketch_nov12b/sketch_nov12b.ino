dabb33f25d8f0a4dca5a6b2db7e880895a2ed811
stephen7522 <stephen7522@gmail.com>
2016-11-12 09:59:26 -0500

    First measure light sensor values

---
+++ sketch_nov12b/sketch_nov12b.ino
@@ -0,0 +1,25 @@
+#include <Sparki.h>
+void setup() {
+
+}
+
+void loop() {
+ 
+ int lineLeft   = sparki.lineLeft();   // measure the left IR sensor
+ int lineCenter = sparki.lineCenter(); // measure the center IR sensor
+ int lineRight  = sparki.lineRight();  // measure the right IR sensor
+ sparki.clearLCD(); // wipe the screen
+ 
+  sparki.print("Line Left: "); // show left line sensor on screen
+  sparki.println(lineLeft);
+ 
+  sparki.print("Line Center: "); // show center line sensor on screen
+  sparki.println(lineCenter);
+ 
+  sparki.print("Line Right: "); // show right line sensor on screen
+  sparki.println(lineRight);
+ 
+  sparki.updateLCD(); // display all of the information written to the screen
+ 
+  delay(100); // wait 0.1 seconds
+}
